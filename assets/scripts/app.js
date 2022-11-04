b63665f0bd759c53d4e32b76a9e39a7cab1a952f
nischay <nischey.me008@gmail.com>
2022-11-04 17:47:44 +0530

    4th november

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,38 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-outputResult(currentResult, calculationDescription);
+
+function getUserInput(){
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator,resultBeforecal,calcNumber){
+    const callDescription=`${resultBeforecal}+${operator}`;
+}
+
+function add(){
+    const enterNumber=getUserInput();
+    currentResult=currentResult+enterNumber;
+    outputResult(currentResult,callDescription);
+}
+
+function substract(){
+    const enterNumber=getUserInput();
+    const callDescription=`${currentResult}-${enterNumber}`;
+    currentResult=currentResult+enterNumber;
+    outputResult(currentResult,callDescription);
+}
+function multiply(){
+    const enterNumber=getUserInput();
+    const callDescription=`${currentResult}*${enterNumber}`;
+    currentResult=currentResult+enterNumber;
+    outputResult(currentResult,callDescription);
+}
+function divide(){
+    const enterNumber=getUserInput();
+    const callDescription=`${currentResult}/${enterNumber}`;
+    currentResult=currentResult+enterNumber;
+    outputResult(currentResult,callDescription);
+}
+addBtn.addEventListener('click',add)
+
+ 
