3f23b83778c195e430c8d610b00a666f90c9e827
nischay <nischey.me008@gmail.com>
2024-02-02 01:40:06 +0530

    Added on 2nd Feb 2024

--- Practice/Simple Calculator App/assets/scripts/app.js
+++ Practice/Simple Calculator App/assets/scripts/app.js
@@ -1,7 +1,20 @@
-let currenResult = 0;
-currenResult = currenResult + 10;
-console.log(currenResult);
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+const additionResult = add(1, 2);
 
 
 
-outputResult(currenResult, '');
+
+currentResult = additionResult
+
+let calculationDescription = `(${defaultResult}+10) *3/2-1`;
+
+outputResult(currentResult, calculationDescription);
+
