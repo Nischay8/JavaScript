c0c0f4797cfc0d9bc5b62f7ced180a6bec154962
nischay <nischey.me008@gmail.com>
2024-02-09 01:33:46 +0530

    Added on 09 feb 2024

--- Practice/Simple Calculator App/assets/scripts/app.js
+++ Practice/Simple Calculator App/assets/scripts/app.js
@@ -12,14 +12,27 @@ function getUserNumberInput() {
 }
 
 
+function writeToLog(operationIdentifier, previousResult, operationNumber, NewResult) {
+    const logEntry = {
+        operation: operationIdentifier,
+        preResult: previousResult,
+        number: operationNumber,
+        result: NewResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialNumber = currentResult;
     const calcDescription = `${currentResult} + ${enteredNumber}`;
     currentResult = currentResult + parseInt(userInput.value);
     CreateAndWriteOutput('+', initialNumber, enteredNumber);
-    logEntries.push(enteredNumber);
-    console.log(logEntries);
+
+    writeToLog('ADD', initialNumber, enteredNumber, currentResult);
+    // logEntries.push(enteredNumber);
+    // console.log(logEntries);
 }
 
 
@@ -29,6 +42,7 @@ function subtact() {
     const calcDescription = `${currentResult} - ${enteredNumber}`;
     currentResult = currentResult - parseInt(userInput.value);
     CreateAndWriteOutput('-', initialNumber, enteredNumber);
+    writeToLog('Subtract', initialNumber, enteredNumber, currentResult);
 }
 
 
@@ -38,6 +52,7 @@ function multiply() {
     const calcDescription = `${currentResult} * ${enteredNumber}`;
     currentResult = currentResult * parseInt(userInput.value);
     CreateAndWriteOutput('*', initialNumber, enteredNumber);
+    writeToLog('Multiply', initialNumber, enteredNumber, currentResult);
 }
 
 
@@ -47,6 +62,7 @@ function divide() {
     const calcDescription = `${currentResult} / ${enteredNumber}`;
     currentResult = currentResult / parseInt(userInput.value);
     CreateAndWriteOutput('*', initialNumber, enteredNumber);
+    writeToLog('Divide', initialNumber, enteredNumber, currentResult);
 }
 
 
