d70d9b91fbfe17e9593e73036c4753033c382e30
nischay <nischey.me008@gmail.com>
2024-02-25 01:40:22 +0530

    Added on 25 Feb 2024

---
+++ Practice/Function_Practice/app.js
@@ -0,0 +1,7 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    console.log('Game is Starting Up..!!!');
+}
+
+startGameBtn.addEventListener('click', startGame);
