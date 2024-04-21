0c5ac62d65f4e510e04c5e4ccf11111d1ed640cd
nischay <nischey.me008@gmail.com>
2024-04-21 19:43:52 +0530

    Added on 21 April 2024

---
+++ JavaScript_CODE/05_function.js
@@ -0,0 +1,9 @@
+// console.log("Nischay");
+
+function sayMyName() {
+    console.log("Nischay");
+    console.log("Sinha");
+    console.log("Chandigarh University");
+}
+
+sayMyName();
---
+++ JavaScript_CODE/scope_v2.js
@@ -0,0 +1,26 @@
+// function one() {
+//     const username = "Hitesh";
+//     function two() {
+//         const website = "Youtube";
+//         console.log(username);
+//     }
+
+//     console.log(website);
+
+//     two();
+// }
+
+// one();
+
+
+if (true) {
+    const username = "hitesh";
+    if (username === "hitesh") {
+        const website = "youtube";
+        console.log(username + website);
+    }
+
+    // console.log(website);  //Cannot Access Because of Scope
+}
+
+// console.log(username); Cannot Access Because of Scope;
