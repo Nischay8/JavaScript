b69f476968612124c94dbb1a7cb6842cb766d270
nischay <nischey.me008@gmail.com>
2024-04-21 20:59:11 +0530

    Added on 21st April 2024

---
+++ JavaScript_CODE/arrow_function.js
@@ -0,0 +1,44 @@
+// This code COntains Meaning of THis and Arrow Function
+
+const user = {
+    username: "hitesh",
+    price: 99,
+    welcomeMessage: function () {
+        console.log(`${this.username},welcome to website`);
+        // console.log(this);
+    }
+
+
+}
+
+
+// user.welcomeMessage();
+// user.username = "Nischay";
+// user.welcomeMessage();
+
+
+// console.log(this);
+
+
+// function chai() {
+//     console.log(this);
+// }
+
+// chai();
+
+
+// const chai=function(){
+//     let username="Hitesh"
+//     console.log(this.username);
+// }
+
+// chai();
+
+
+
+const chai = () => {
+    let username = "hitesh";
+    console.log(this);
+}
+
+chai();
